3c27f8c35746e9485674232eb80cf6aeecf264d7
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-04-25 14:14:30 +0900

    init

---
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -0,0 +1,47 @@
+package com.ll.gramgram.base.initData;
+
+import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
+import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
+import com.ll.gramgram.boundedContext.member.entity.Member;
+import com.ll.gramgram.boundedContext.member.service.MemberService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
+
+@Configuration
+@Profile({"dev", "test"})
+public class NotProd {
+    @Bean
+    CommandLineRunner initData(
+            MemberService memberService,
+            InstaMemberService instaMemberService,
+            LikeablePersonService likeablePersonService
+    ) {
+        return new CommandLineRunner() {
+            @Override
+            @Transactional
+            public void run(String... args) throws Exception {
+                Member memberAdmin = memberService.join("admin", "1234").getData();
+                Member memberUser1 = memberService.join("user1", "1234").getData();
+                Member memberUser2 = memberService.join("user2", "1234").getData();
+                Member memberUser3 = memberService.join("user3", "1234").getData();
+                Member memberUser4 = memberService.join("user4", "1234").getData();
+                Member memberUser5 = memberService.join("user5", "1234").getData();
+
+                Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2731659195").getData();
+                Member memberUser7ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__103240209825008011930").getData();
+                Member memberUser7ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__ibvGetSExxTuzr2Zy_SBG7KNLQXhoW1aynxoiwDSP6c").getData();
+
+                instaMemberService.connect(memberUser2, "insta_user2", "M");
+                instaMemberService.connect(memberUser3, "insta_user3", "W");
+                instaMemberService.connect(memberUser4, "insta_user4", "M");
+                instaMemberService.connect(memberUser5, "insta_user5", "W");
+
+                likeablePersonService.like(memberUser3, "insta_user4", 1);
+                likeablePersonService.like(memberUser3, "insta_user100", 2);
+            }
+        };
+    }
+}
