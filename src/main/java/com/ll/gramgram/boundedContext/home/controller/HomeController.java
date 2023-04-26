e9d773bda8b14de68245e0b77da03e84fb3ca4e4
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-04-26 15:47:20 +0900

    [feat] 도메인/(인스타아이디) 로 요청하면 해당 유저의 호감 정보를 보여줌

--- src/main/java/com/ll/gramgram/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/ll/gramgram/boundedContext/home/controller/HomeController.java
@@ -1,23 +1,44 @@
 package com.ll.gramgram.boundedContext.home.controller;
 
 import com.ll.gramgram.base.rq.Rq;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Enumeration;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final Rq rq;
+    private final InstaMemberService instaMemberService;
 
     @GetMapping("/")
     public String showMain() {
         return "usr/home/main";
+    }
+
+    @GetMapping("/{instaMemberUsername}")
+    public String showInfo(@PathVariable String instaMemberUsername, Model model) {
+        Optional<InstaMember> oInstaMember = instaMemberService.findByUsername(instaMemberUsername);
+
+        InstaMember instaMember = null;
+
+        if(oInstaMember.isPresent()) {
+            instaMember = oInstaMember.get();
+        }
+
+        model.addAttribute("member", instaMember);
+
+        return "usr/home/info";
     }
 
     @GetMapping("/usr/debugSession")
